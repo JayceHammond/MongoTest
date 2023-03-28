07b0f9ae42a8196bd27fb4b43d85d4f4448ac4f4
JayceHammond <hjayce6@gmail.com>
2023-03-28 11:56:06 -0500

    connection string fixed

--- testmongo.js
+++ testmongo.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri =
-  "mongodb+srv://JayceDB:<yhlnSd5eupLkZ55N>@cmps415.rfhvwh1.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://<JayceDB>:<yhlnSd5eupLkZ55N>@cmps415.rfhvwh1.mongodb.net/?retryWrites=true&w=majority";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require("express");
