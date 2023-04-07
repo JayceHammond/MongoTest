e0967e22be41345a002dece7d6c1951a2cace605
JayceHammond <hjayce6@gmail.com>
2023-04-07 13:24:48 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -66,17 +66,21 @@ app.get("/rest/list", function (req, res) {
       const query = {_id: {$gt: 0}};
 
       const options = {
-        projection: {_id: 1, createdAt: 1, updatedAt: 1, type: 1, subject: 1, Description: 1, priority: 1, status: 1, recipient: 1, submitter: 1, asignee_ID: 1, follower_IDs: 1, tags: 1}
+        projection: {_id: 1, createdAt: 1, updatedAt: 1, type: 1, subject: 1, Description: 1, 
+        priority: 1, status: 1, recipient: 1, submitter: 1, asignee_ID: 1, follower_IDs: 1, tags: 1}
       }
       
-      const cursor = ticket.find(query, options);
+      const cursor = ticket.find(query, options).toArray();
 
       if((await ticket.countDocuments(query)) == 0){
         console.log("No docs found");
       }
       
-      await cursor.forEach(res.send());
+      await cursor.forEach(console.dir);
 
+      res.send("Found tehse: " + JSON.stringify(cursor));
+
+      //res.send(cursor.forEach(x => x.values));
     } finally {
       await client.close();
     }
