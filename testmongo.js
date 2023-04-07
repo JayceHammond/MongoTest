e6ff7643eb03fb9e0e3c06c9054202548507c770
JayceHammond <hjayce6@gmail.com>
2023-04-07 12:57:58 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -74,7 +74,7 @@ app.get("/rest/list", function (req, res) {
       if((await ticket.countDocuments(query)) == 0){
         console.log("No docs found");
       }
-
+      res.send("Found these: " + JSON.stringify(cursor));
       await cursor.forEach(console.dir);
 
     } finally {
