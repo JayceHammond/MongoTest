4aff20469dfd4e18924ae4256cbb9a77e697a66a
JayceHammond <hjayce6@gmail.com>
2023-04-07 13:43:39 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -78,7 +78,7 @@ app.get("/rest/list", function (req, res) {
       
       await cursor.forEach(console.dir);
 
-      res.send("Found these: " + JSON.stringify(cursor.forEach().toArray()));
+      res.send("Found these: " + JSON.stringify(cursor.forEach(console.dir).toArray()));
     } finally {
       await client.close();
     }
